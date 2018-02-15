7075e4bc763ff5ab5bd2a60e52473713d916b9d9
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-15 15:29:22 -0800

    Setup directory scafolding

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 'use strict';
 
+
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const debug = require('debug')('giggle: server.js');
 const PORT = process.env.PORT || 3500;
 const app = express();
+
+
+
+app.use(cors());
+
 
 app.listen(PORT, () => {
 	debug('Server active on port: ', PORT);
---
+++ test/userRouter-test.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const User = require('../models/user.js');
+const expect = require('chai').expect;
+const superagent = require('superagent');
+
